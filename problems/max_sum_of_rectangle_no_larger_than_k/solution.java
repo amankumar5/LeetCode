f49e14c5c075c77d141d3253639b3c560f564198
amankumar5 <43738186+amankumar5@users.noreply.github.com>
2021-07-04 07:35:37 +0000

    Sync LeetCode submission - Max Sum of Rectangle No Larger Than K (java)

---
+++ problems/max_sum_of_rectangle_no_larger_than_k/solution.java
@@ -0,0 +1,45 @@
+class Solution {
+    static int findMaxofRow(int a[],int maxSum)
+    {
+        int size = a.length;
+        int max_so_far = Integer.MIN_VALUE;
+ 
+        for (int i = 0; i < size; i++)
+        {
+            int temp_sum =0;
+            for(int j=i;j<size;j++)
+            {
+               temp_sum+=a[j];
+               if(temp_sum<=maxSum && temp_sum>max_so_far)
+               max_so_far = temp_sum;
+            }
+        }
+        return max_so_far;
+    }
+    public int maxSumSubmatrix(int[][] matrix, int k) {
+        int rowSize = matrix.length;
+        int columnSize = matrix[0].length;
+        int maxSum = Integer.MIN_VALUE;
+        for(int i=0; i<rowSize; i++)
+        {
+            int[] maxOfRow = new int[columnSize];
+            for(int j=i;j<rowSize; j++)
+            {
+                for(int rowItr = 0; rowItr<columnSize; rowItr++)
+                {
+                    maxOfRow[rowItr] +=matrix[j][rowItr]; 
+                }
+               
+            int tempMax = findMaxofRow(maxOfRow,k);
+            if(tempMax > maxSum )
+            {
+                
+                maxSum = tempMax;
+            }
+            }
+            
+            
+        }
+        return maxSum;
+    }
+}
