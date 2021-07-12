6261ff7de9d3593e0f8cd99076e200495a57f72c
amankumar5 <43738186+amankumar5@users.noreply.github.com>
2021-07-12 10:27:58 +0000

    Sync LeetCode submission - Isomorphic Strings (java)

---
+++ problems/isomorphic_strings/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        Map<Character, Character> map1 = new HashMap<Character, Character>();
+        for(int i = 0; i < s.length(); i++)
+        {   
+            if(map1.containsKey(t.charAt(i)))
+            {
+                if(map1.get(t.charAt(i))!= s.charAt(i))
+                    return false;
+            }
+            else
+            {
+                map1.put(t.charAt(i),s.charAt(i));
+            }
+            if(map.containsKey(s.charAt(i)))
+            {
+                if(map.get(s.charAt(i))!= t.charAt(i))
+                    return false;
+            }
+            else
+            {
+                map.put(s.charAt(i),t.charAt(i));
+            }
+        }
+        return true;
+    }
+}
