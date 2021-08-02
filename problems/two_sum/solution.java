03ecc932242f1d6ed9ac8ae4ead313903b448a74
amankumar5 <43738186+amankumar5@users.noreply.github.com>
2021-08-02 15:07:25 +0000

    Sync LeetCode submission - Two Sum (java)

--- problems/two_sum/solution.java
+++ problems/two_sum/solution.java
@@ -1,22 +1,9 @@
-import java.util.Arrays;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        for(int i=0;i<nums.length;i++)
-        {
-            int diff=target-nums[i];
-            for(int j=i+1; j<nums.length;j++)
-            {
-                System.out.println(i+" "+j+" "+diff);
-                System.out.println(nums[j]);
-                if(nums[j]==diff){
-                 int[] intArray = new int[2];
-                intArray[0]=i;
-                intArray[1]=j;
-                return intArray;
-                }
-            }
-        }
-        return new int[2] ;
+        for(int i = 0;i<nums.length-1;i++)
+            for(int j =i+1;j<nums.length;j++)
+                if(nums[i]+nums[j]==target)
+                    return new int[] {i,j};
+        return new int[] {-1,-1};
     }
 }
